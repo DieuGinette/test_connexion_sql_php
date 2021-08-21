196e0f0d3e8b62a6a8fd8cb4f5dbf87e21e02797
DieuGinette <32375850+DieuGinette@users.noreply.github.com>
2021-08-21 10:26:07 +0200

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "db_username";
+$password = "db_password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
